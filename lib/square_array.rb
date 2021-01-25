590e6f5b17099abae84bf10fd03515c5a2c14f4b
Paige Kelly <pckelly.co@gmail.com>
2021-01-25 03:01:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   count = 0
   new_array = []
-  while count < array.length do |index|
-    ele = array[index]
+  while count < array.length do
+    ele = array[count]
     new_array.push(ele ** 2)
     count += 1
   end
